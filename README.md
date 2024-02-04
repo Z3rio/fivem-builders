91dc53df9a3930dd28997a87b9299179c740c0bf
Zerio <neoboij@gmail.com>
2024-02-04 14:38:00 +0100

    feat: add yarn ignore

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# fivem-builders
+# FiveM Builders
+
 An more maintained version of the yarn/webpack builders for FiveM.
+
+## Main Changes
+
+### Let packages be ignored by yarn
+
+Simply add `"fivemIgnore": true,` to the resource's `package.json` file and it
+shall be completely ignored by yarn.
--- yarn/yarn_builder.js
+++ yarn/yarn_builder.js
@@ -1,81 +1,99 @@
-const path = require('path');
-const fs = require('fs');
-const child_process = require('child_process');
+const path = require("path");
+const fs = require("fs");
+const child_process = require("child_process");
 let buildingInProgress = false;
-let currentBuildingModule = '';
+let currentBuildingModule = "";
 
 const initCwd = process.cwd();
 const trimOutput = (data) => {
-	return `[yarn]\t` + data.toString().replace(/\s+$/, '');
-}
+  return `[yarn]\t` + data.toString().replace(/\s+$/, "");
+};
 
 const yarnBuildTask = {
-	shouldBuild(resourceName) {
-		try {
-			const resourcePath = GetResourcePath(resourceName);
-			
-			const packageJson = path.resolve(resourcePath, 'package.json');
-			const yarnLock = path.resolve(resourcePath, '.yarn.installed');
-			
-			const packageStat = fs.statSync(packageJson);
-			
-			try {
-				const yarnStat = fs.statSync(yarnLock);
-				
-				if (packageStat.mtimeMs > yarnStat.mtimeMs) {
-					return true;
-				}
-			} catch (e) {
-				// no yarn.installed, but package.json - install time!
-				return true;
-			}
-		} catch (e) {
-			
-		}
-		
-		return false;
-	},
-	
-	build(resourceName, cb) {
-		(async () => {
-			while (buildingInProgress && currentBuildingModule !== resourceName) {
-				console.log(`yarn is currently busy: we are waiting to compile ${resourceName}`);
-				await sleep(3000);
-			}
-			buildingInProgress = true;
-			currentBuildingModule = resourceName;
-			const proc = child_process.fork(
-				require.resolve('./yarn_cli.js'),
-				['install', '--ignore-scripts', '--cache-folder', path.join(initCwd, 'cache', 'yarn-cache'), '--mutex', 'file:' + path.join(initCwd, 'cache', 'yarn-mutex')],
-				{
-					cwd: path.resolve(GetResourcePath(resourceName)),
-					stdio: 'pipe',
-				});
-			proc.stdout.on('data', (data) => console.log(trimOutput(data)));
-			proc.stderr.on('data', (data) => console.error(trimOutput(data)));
-			proc.on('exit', (code, signal) => {
-				setImmediate(() => {
-					if (code != 0 || signal) {
-						buildingInProgress = false;
-						currentBuildingModule = '';
-						cb(false, 'yarn failed!');
-						return;
-					}
+  shouldBuild(resourceName) {
+    try {
+      const resourcePath = GetResourcePath(resourceName);
 
-					const resourcePath = GetResourcePath(resourceName);
-					const yarnLock = path.resolve(resourcePath, '.yarn.installed');
-					fs.writeFileSync(yarnLock, '');
+      const packageJson = path.resolve(resourcePath, "package.json");
+      const packageJsonContents = fs.readFileSync(packageJson, {
+        encoding: "utf8",
+        flag: "r",
+      });
+      if (packageJsonContents) {
+        const parsedPackageJson = JSON.parse(packageJsonContents);
 
-					buildingInProgress = false;
-					currentBuildingModule = '';
-					cb(true);
-				});
-			});
-		})();
-	}
+        if (parsedPackageJson.fivemIgnore !== true) {
+          const yarnLock = path.resolve(resourcePath, ".yarn.installed");
+
+          const packageStat = fs.statSync(packageJson);
+
+          try {
+            const yarnStat = fs.statSync(yarnLock);
+
+            if (packageStat.mtimeMs > yarnStat.mtimeMs) {
+              return true;
+            }
+          } catch (e) {
+            // no yarn.installed, but package.json - install time!
+            return true;
+          }
+        }
+      }
+    } catch (e) { }
+
+    return false;
+  },
+
+  build(resourceName, cb) {
+    (async () => {
+      while (buildingInProgress && currentBuildingModule !== resourceName) {
+        console.log(
+          `yarn is currently busy: we are waiting to compile ${resourceName}`,
+        );
+        await sleep(3000);
+      }
+      buildingInProgress = true;
+      currentBuildingModule = resourceName;
+      const proc = child_process.fork(
+        require.resolve("./yarn_cli.js"),
+        [
+          "install",
+          "--ignore-scripts",
+          "--cache-folder",
+          path.join(initCwd, "cache", "yarn-cache"),
+          "--mutex",
+          "file:" + path.join(initCwd, "cache", "yarn-mutex"),
+        ],
+        {
+          cwd: path.resolve(GetResourcePath(resourceName)),
+          stdio: "pipe",
+        },
+      );
+      proc.stdout.on("data", (data) => console.log(trimOutput(data)));
+      proc.stderr.on("data", (data) => console.error(trimOutput(data)));
+      proc.on("exit", (code, signal) => {
+        setImmediate(() => {
+          if (code != 0 || signal) {
+            buildingInProgress = false;
+            currentBuildingModule = "";
+            cb(false, "yarn failed!");
+            return;
+          }
+
+          const resourcePath = GetResourcePath(resourceName);
+          const yarnLock = path.resolve(resourcePath, ".yarn.installed");
+          fs.writeFileSync(yarnLock, "");
+
+          buildingInProgress = false;
+          currentBuildingModule = "";
+          cb(true);
+        });
+      });
+    })();
+  },
 };
 
 function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
-RegisterResourceBuildTaskFactory('yarn', () => yarnBuildTask);
+RegisterResourceBuildTaskFactory("yarn", () => yarnBuildTask);
